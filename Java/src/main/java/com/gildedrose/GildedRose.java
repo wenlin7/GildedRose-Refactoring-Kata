e180d04f3b0ef8b656fecbb8486f0c5bc10f1d41
wenlin7 <wenlin7.chen@gmail.com>
2020-08-11 16:34:26 +0800

    每個品項的邏輯抽出成method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,36 +19,48 @@ class GildedRose {
         }
 
         if (item.name.equals("Aged Brie")) {
-            item.sellIn = item.sellIn - 1;
-            
-            increaseQuality(item);
-            if (item.sellIn < 0) {
-                increaseQuality(item);
-            }
+            updateAgedBrie(item);
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.sellIn = item.sellIn - 1;
-            
-            increaseQuality(item);
-
-            if (item.sellIn < 10) {
-                increaseQuality(item);
-            }
-
-            if (item.sellIn < 5) {
-                increaseQuality(item);
-            }
-            
-            if (item.sellIn < 0) {
-                item.quality = item.quality - item.quality;
-            }
+            updateBackstagePasses(item);
 
         } else {
-            item.sellIn = item.sellIn - 1;
-            
+            updateNormal(item);
+        }
+    }
+
+    private void updateNormal(Item item) {
+        item.sellIn = item.sellIn - 1;
+        
+        decreaseQuality(item);
+        if (item.sellIn < 0) {
             decreaseQuality(item);
-            if (item.sellIn < 0) {
-                decreaseQuality(item);
-            }
+        }
+    }
+
+    private void updateBackstagePasses(Item item) {
+        item.sellIn = item.sellIn - 1;
+        
+        increaseQuality(item);
+
+        if (item.sellIn < 10) {
+            increaseQuality(item);
+        }
+
+        if (item.sellIn < 5) {
+            increaseQuality(item);
+        }
+        
+        if (item.sellIn < 0) {
+            item.quality = item.quality - item.quality;
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.sellIn = item.sellIn - 1;
+        
+        increaseQuality(item);
+        if (item.sellIn < 0) {
+            increaseQuality(item);
         }
     }
 
