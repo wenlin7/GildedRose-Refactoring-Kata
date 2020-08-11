8a4b2b02c53ecb0401845e57c14d9bf3a591eaf0
wenlin7 <wenlin7.chen@gmail.com>
2020-08-11 16:57:32 +0800

    modified Item and create 4 subclass

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -13,6 +13,22 @@ public class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
+    
+    public void update() {
+        
+    }
+    
+    public void decreaseQuality() {
+        if (this.quality > 0) {
+            this.quality = this.quality - 1;
+        }
+    }
+
+    public void increaseQuality() {
+        if (this.quality < 50) {
+            this.quality = this.quality + 1;
+        }
+    }
 
    @Override
    public String toString() {
