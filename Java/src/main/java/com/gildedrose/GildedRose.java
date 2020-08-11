2f35325149414b3ee1837cc38ae08667b4280de2
wenlin7 <wenlin7.chen@gmail.com>
2020-08-11 15:58:05 +0800

    Sulfuras 獨立拉出成 if 判斷式

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,6 +14,10 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+
         if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -35,17 +39,10 @@ class GildedRose {
                 }
             }
         } else if (item.quality > 0) {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            } else {
-                item.quality = item.quality - 1;
-            }
+            item.quality = item.quality - 1;
         }
 
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            
-        } else {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
@@ -57,11 +54,7 @@ class GildedRose {
                     item.quality = item.quality - item.quality;
                 } else {
                     if (item.quality > 0) {
-                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            
-                        } else {
-                            item.quality = item.quality - 1;
-                        }
+                        item.quality = item.quality - 1;
                     }
                 }
             }
